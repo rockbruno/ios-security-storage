c5e8ebccae0302d257c633e8f72b01ce99c5390d
bruno-rocha-movile <bruno.rocha@movile.com>
2017-07-04 18:03:57 -0300

    Initial

---
+++ StorageTest/StorageViewController.m
@@ -0,0 +1,51 @@
+//
+//  StorageViewController.m
+//  techtalkProj
+//
+//  Created by Bruno Rocha on 6/21/17.
+//  Copyright © 2017 Bruno Rocha. All rights reserved.
+//
+
+#import "StorageViewController.h"
+#import "SAMKeychain.h"
+
+@interface StorageViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+@end
+
+@implementation StorageViewController
+
+- (NSManagedObjectContext *)managedObjectContext
+{
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+
+- (IBAction)defaults:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setValue:_textField.text forKey:@"SecurityProjDefaultsKey"];
+    [defaults synchronize];
+}
+
+- (IBAction)keychain:(id)sender {
+    [SAMKeychain setPassword:_textField.text forService:@"SecurityTestService" account:@"SecurityTestAccount"];
+}
+
+- (IBAction)coreData:(id)sender {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    NSManagedObject *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"StorageData" inManagedObjectContext:context];
+    [newDevice setValue:_textField.text forKey:@"value"];
+    NSError *error = nil;
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+}
+
+@end
---
+++ StorageTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyContacts
+//
+//  Created by Charles Konkol on 11/10/13.
+//  Copyright (c) 2013 Chuck Konkol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyContactsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyContactsAppDelegate class]));
+    }
+}
